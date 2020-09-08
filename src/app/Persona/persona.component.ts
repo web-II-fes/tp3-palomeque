57229ebf74a0d384d03ae5bcb97301318777b0a4
ivo <ivan.ap9117@gmail.com>
2020-09-08 10:44:46 -0300

    Creación formulario con carga y edición de persona

---
+++ src/app/Persona/persona.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validator, Validators} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { PersonaService } from './../servicios/persona.service'; 
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css']
+})
+export class PersonaComponent implements OnInit {
+
+   formCliente : FormGroup;
+   personas : any[] = [];
+   idPersona : any;
+
+  constructor(private fb : FormBuilder, private router : Router, private personaService: PersonaService) { }
+
+  ngOnInit() {
+    debugger;		
+      this.initForm();
+      this.getPersona();
+  }
+
+  nombreControl = new FormControl('User');
+
+   initForm(){
+  //    this.formCliente = this.fb.group({
+    this.formCliente = this.fb.group({
+      nombre : ['', Validators.required],
+      apellido : ['', Validators.required],
+      edad : ['', Validators.required],
+     });
+    }
+
+    getPersona(){
+      this.personaService.getPersonas().subscribe((personas: any) =>{
+        this.personas = personas;
+      });
+    }
+
+    editarPersona(persona: any){
+      this.idPersona = persona._id;
+      this.formCliente.patchValue({
+        nombre: persona.nombre,
+        apellido: persona.apellido,
+        edad: persona.edad
+      });
+    }
+
+    //borrarPersona()
+
+   enviar(){
+    if(this.idPersona){
+      this.personaService.editarPersona(this.idPersona, this.formCliente.value).subscribe((persona) =>{
+        console.log("Persona Editada: ", persona);
+      });
+  } else {
+    this.personaService.guardarPersona(this.formCliente.value).subscribe((persona) => {
+      console.log("Persona Nueva: ", persona);
+    });
+    }
+   };  
+  
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonaComponent } from './Persona/persona.component';
+
+
+// import {MatTableModule} from '@angular/material/table';
+// import {MatMenuModule} from '@angular/material/menu';
+// import {MatButtonModule} from '@angular/material/button';
+ import {MatFormFieldModule} from '@angular/material/form-field';
+ import {MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+
+//import {MatSelectModule} from '@angular/material/select';
+import{ FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { PersonaService } from './servicios/persona.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaComponent
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule
+  
   ],
-  providers: [],
+  providers: [PersonaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
